cd5a0e97531684fbe952fb9eee206dc9ae87156a
Akshay Kumar <premiumfileprovider@gmail.com>
2021-02-26 14:20:58 +0530

    help function

---
+++ helper_funcs/chat_base.py
@@ -0,0 +1,25 @@
+import os
+
+# the secret configuration specific things
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+# the Strings used for this "thing"
+from translation import Translation
+
+from pyrogram import Client, filters
+
+# the Telegram trackings
+from chatbase import Message
+
+
+def TRChatBase(chat_id, message_text, intent):
+    msg = Message(api_key=Config.CHAT_BASE_TOKEN,
+              platform="Telegram",
+              version="1.3",
+              user_id=chat_id,
+              message=message_text,
+              intent=intent)
+    resp = msg.send()
